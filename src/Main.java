a2278854bc0a5cc5b0dd5045f09b63b0bb5e0e75
B-e-l-o-u-s-o-v <evgeniy.belousov.2000@mail.ru>
2022-11-10 18:28:47 +0600

    Дополнительное задание выполнено

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,15 @@ public class Main
     public static void main(String[] args)
     {
         Container container = new Container();
-        Container.count += 7843;
+        container.count += 7843;
+
         System.out.println(Main.sumDigits(12345));
         System.out.println(Main.sumDigits(10));
         System.out.println(Main.sumDigits(5059191));
+
+        System.out.println(Main.sumDigitsFromChar(12345));
+        System.out.println(Main.sumDigitsFromChar(10));
+        System.out.println(Main.sumDigitsFromChar(5059191));
     }
 
     public static int sumDigits(int number)
@@ -16,6 +21,18 @@ public class Main
 
         for (var i = 0; i < str.length(); i++) {
             sum += Integer.parseInt(String.valueOf(str.charAt(i)));
+        }
+
+        return sum;
+    }
+
+    public static int sumDigitsFromChar(int number)
+    {
+        var str = String.valueOf(number);
+        var sum = 0;
+
+        for (var i = 0; i < str.length(); i++) {
+            sum += Character.getNumericValue(str.charAt(i));
         }
 
         return sum;
